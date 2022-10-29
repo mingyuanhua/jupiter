13e1660323ad34e86eeb8ddffbb9375f9ba7adf1
mingyuanhua <m.y.hua@hotmail.com>
2022-10-30 02:04:24 +0800

    Add Game Implementation with Twitch API

--- src/main/java/com/example/jupiter/controller/GameController.java
+++ src/main/java/com/example/jupiter/controller/GameController.java
@@ -1,43 +1,36 @@
 package com.example.jupiter.controller;
 
 import com.example.jupiter.service.GameService;
+import com.example.jupiter.service.TwitchException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Controller
 public class GameController {
+
     @Autowired
     private GameService gameService;
 
-//    private final GameService gameService;
-//    @Autowired
-//    public GameController(GameService gameService) {
-//        this.gameService = gameService;
-//    }
-
-    // /game?game_name=whatever
-    // /game
     @RequestMapping(value = "/game", method = RequestMethod.GET)
-    public void getGame(@RequestParam(value = "game_name", required = false) String
-                        gameName, HttpServletResponse response) throws IOException {
-        String fakeName = gameService.fakeGame();
-        response.getWriter().write(fakeName);
+    public void getGame(@RequestParam(value = "game_name", required = false) String gameName, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("application/json;charset=UTF-8");
+        try {
+            // Return the dedicated game information if gameName is provided in the request URL, otherwise return the top x games.
+            if (gameName != null) {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(gameService.searchGame(gameName)));
+            } else {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(gameService.topGames(0)));
+            }
+        } catch (TwitchException e) {
+            throw new ServletException(e);
+        }
     }
-
-//    @RequestMapping(value = "/search", method = RequestMethod.GET)
-//    @ResponseBody
-//    public String search(@RequestParam(value = "lon", required = false) Double lon, @RequestParam(value = "lat", required = false) Double lat, HttpServletResponse response) throws IOException {
-//        System.out.println(lon);
-//        return "hello search lat: " + lat + ", lon: " + lon;
-//    }
-//
-//    @RequestMapping(value = "/restaurant/{id}/menu", method = RequestMethod.GET)
-//    public void searchMenu(@PathVariable("id") int id) {
-//
-//    }
-
 }
