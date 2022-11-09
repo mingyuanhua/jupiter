4cc58741dee144438afbdb83cdafba732c7bc67d
mingyuanhua <m.y.hua@hotmail.com>
2022-11-09 13:04:19 +0800

    Add Favorite with Auth

--- src/main/java/com/example/jupiter/controller/FavoriteController.java
+++ src/main/java/com/example/jupiter/controller/FavoriteController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,38 +26,38 @@ public class FavoriteController {
     @RequestMapping(value = "/favorite", method = RequestMethod.POST)
     public void setFavoriteItem(@RequestBody FavoriteRequestBody requestBody,
                                 HttpServletRequest request, HttpServletResponse response) {
-//        HttpSession session = request.getSession(false);
-//        if (session == null) {
-//            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-//            return;
-//        }
-//        String userId = (String) session.getAttribute("user_id");
-        String userId = "1111";
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
+        String userId = (String) session.getAttribute("user_id");
+        //String userId = "1111";
         favoriteService.setFavoriteItem(userId, requestBody.getFavoriteItem());
     }
 
     @RequestMapping(value = "/favorite", method = RequestMethod.DELETE)
     public void unsetFavoriteItem(@RequestBody FavoriteRequestBody requestBody, HttpServletRequest request, HttpServletResponse response) {
-//        HttpSession session = request.getSession(false);
-//        if (session == null) {
-//            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-//            return;
-//        }
-//        String userId = (String) session.getAttribute("user_id");
-        String userId = "1111";
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
+        String userId = (String) session.getAttribute("user_id");
+        //String userId = "1111";
         favoriteService.unsetFavoriteItem(userId, requestBody.getFavoriteItem().getId());
     }
 
     @RequestMapping(value = "/favorite", method = RequestMethod.GET)
     @ResponseBody
     public Map<String, List<Item>> getFavoriteItem(HttpServletRequest request, HttpServletResponse response) {
-//        HttpSession session = request.getSession(false);
-//        if (session == null) {
-//            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-//            return new HashMap<>();
-//        }
-//        String userId = (String) session.getAttribute("user_id");
-        String userId = "1111";
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return new HashMap<>();
+        }
+        String userId = (String) session.getAttribute("user_id");
+        //String userId = "1111";
         return favoriteService.getFavoriteItems(userId);
     }
 }
