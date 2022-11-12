5b379639f2b8eb9eaa107d941917f895a7abb1c6
mingyuanhua <m.y.hua@hotmail.com>
2022-11-13 01:11:47 +0800

    Add Recommendation

--- src/main/java/com/example/jupiter/service/GameService.java
+++ src/main/java/com/example/jupiter/service/GameService.java
@@ -161,7 +161,7 @@ public class GameService {
         return videos;
     }
 
-    private List<Item> searchByType(String gameId, ItemType type, int limit) throws TwitchException {
+    public List<Item> searchByType(String gameId, ItemType type, int limit) throws TwitchException {
         List<Item> items = Collections.emptyList();
 
         switch (type) {
